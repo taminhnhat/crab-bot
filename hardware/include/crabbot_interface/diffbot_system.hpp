2740c458623522c7e35dac3063f929bbabab2adc
taminhnhat <taminhnhat98@gmail.com>
2023-07-25 16:15:39 +0700

    i

---
+++ hardware/include/crabbot_interface/diffbot_system.hpp
@@ -0,0 +1,93 @@
+// Copyright 2021 ros2_control Development Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CRABBOT_INTERFACE__DIFFBOT_SYSTEM_HPP_
+#define CRABBOT_INTERFACE__DIFFBOT_SYSTEM_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/clock.hpp"
+#include "rclcpp/duration.hpp"
+#include "rclcpp/macros.hpp"
+#include "rclcpp/time.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+
+#include "crabbot_interface/visibility_control.h"
+
+namespace crabbot_interface
+{
+class DiffBotSystemHardware : public hardware_interface::SystemInterface
+{
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(DiffBotSystemHardware);
+
+  CRABBOT_INTERFACE_PUBLIC
+  hardware_interface::CallbackReturn on_init(
+    const hardware_interface::HardwareInfo & info) override;
+
+  CRABBOT_INTERFACE_PUBLIC
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  CRABBOT_INTERFACE_PUBLIC
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  CRABBOT_INTERFACE_PUBLIC
+  hardware_interface::return_type on_configure() override;
+
+  CRABBOT_INTERFACE_PUBLIC
+  hardware_interface::return_type on_cleanup() override;
+
+  CRABBOT_INTERFACE_PUBLIC
+  hardware_interface::return_type on_shutdown() override;
+  
+  CRABBOT_INTERFACE_PUBLIC
+  hardware_interface::return_type on_error() override;
+
+  CRABBOT_INTERFACE_PUBLIC
+  hardware_interface::CallbackReturn on_activate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  CRABBOT_INTERFACE_PUBLIC
+  hardware_interface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  CRABBOT_INTERFACE_PUBLIC
+  hardware_interface::return_type read(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+  CRABBOT_INTERFACE_PUBLIC
+  hardware_interface::return_type write(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+private:
+  // Parameters for the DiffBot simulation
+  double hw_start_sec_;
+  double hw_stop_sec_;
+
+  // Store the command for the simulated robot
+  std::vector<double> hw_commands_;
+  std::vector<double> hw_positions_;
+  std::vector<double> hw_velocities_;
+};
+
+}  // namespace crabbot_interface
+
+#endif  // CRABBOT_INTERFACE__DIFFBOT_SYSTEM_HPP_
