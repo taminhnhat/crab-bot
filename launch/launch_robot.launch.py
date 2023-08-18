fc3842c510a36f3b4ac48931eacc5efcc2ae0431
taminhnhat <taminhnhat98@gmail.com>
2023-08-18 17:43:52 +0700

    i

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -97,7 +97,8 @@ def generate_launch_description():
         package='twist_mux',
         executable='twist_mux',
         parameters=[twist_mux_params],
-        remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')]
+        remappings=[
+            ('/cmd_vel_out', '/diffbot_base_controller/cmd_vel_unstamped')]
     )
 
     # Launch them all!
